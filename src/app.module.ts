45a99072417abbfbeeef680d6f6e76d55d443347
Fedekkc <cacacefederico05@gmail.com>
2024-08-13 12:13:57 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LibrosModule } from './libros/libros.module';
+import { LibrosModule } from './libros/libros.controller';
 import { AutoresModule } from './autores/autores.module';
 import { EditorialesModule } from './editoriales/editoriales.module';
-import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [LibrosModule, AutoresModule, EditorialesModule, CategoriasModule],
+  imports: [LibrosModule, AutoresModule, EditorialesModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+// Las operaciones se hacen a través de los servicios
+
+// En los controladores se definen las rutas
